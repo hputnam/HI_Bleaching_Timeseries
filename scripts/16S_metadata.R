74c02f3be060f616ad0c655bfd9cea6c07fe6997
Emma Strand <emma_strand@uri.edu>
2022-01-07 17:42:10 -0500

    metadata script and multiqc images

--- scripts/16S_metadata.R
+++ scripts/16S_metadata.R
@@ -1,3 +1,31 @@
 ## Creating metadata files for 16S pipeline 
 ## Emma Strand created 1/7/2022 
 
+library(plyr)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+
+## 1. Sample manifest file 
+## filelist.csv created during pipeline in andromeda and scp to desktop to work with in R
+
+file_names <- read.csv("~/MyProjects/HI_Bleaching_Timeseries/data/16S/metadata/filelist.csv", header = FALSE) %>% 
+  select(V2) %>% ## reading in filelist as dataframe and only the second column
+  dplyr::rename(`absolute-filepath` = V2) # renaming to match the verbiage of qiime2 
+
+sample_manifest <- file_names # creating a new df based on the original file_names df 
+sample_manifest$path <- "/data/putnamlab/estrand/BleachingPairs_16S/raw_data/" #adding the absolute file path
+
+sample_manifest <- sample_manifest %>% unite(`absolute-filepath`, path, `absolute-filepath`, sep = "") %>% # merging the two columns to complete the file path 
+  mutate(direction = case_when(grepl("R1", `absolute-filepath`) ~ "forward",
+                               grepl("R2", `absolute-filepath`) ~ "reverse")) # creating a new column to state whether forward or reverse based on the R value in the sequence title name 
+
+sample_manifest$`sample-id` <- substr(sample_manifest$`absolute-filepath`, 53, 58) # creating a new column based on the sample id value
+
+sample_manifest <- sample_manifest[, c(3, 1, 2)] # reordering the columns 
+
+sample_manifest %>% write_csv(file = "~/MyProjects/HI_Bleaching_Timeseries/data/16S/metadata/sample_manifest.csv")
+
+## return to terminal to secure copy paste the sample manifest file to bluewaves/andromeda folders 
