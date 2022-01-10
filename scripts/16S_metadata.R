98ac0f93ff4373b809fbe3f0e47ec6dd785a33ae
Emma Strand <emma_strand@uri.edu>
2022-01-10 10:08:41 -0500

    sectioned forward and reverse quality scores

--- scripts/16S_metadata.R
+++ scripts/16S_metadata.R
@@ -31,3 +31,38 @@ sample_manifest %>% write_csv(file = "~/MyProjects/HI_Bleaching_Timeseries/data/
 ## return to terminal to secure copy paste the sample manifest file to bluewaves/andromeda folders 
 
 ## 2. Sample metadata file 
+## sequencing ID and colonyID info downloaded from this google sheet: https://docs.google.com/spreadsheets/d/1lLvCp-RoRiBSGZ4NBPwi6cmZuozmfS20OJ7hBIueldU/edit#gid=0
+collection.summary <- read.csv("~/MyProjects/HI_Bleaching_Timeseries/data/CollectionSummary.csv", header = TRUE) %>%
+  select(-Biopsy., -Fragment.) %>% # removing 2 columns that are not needed for this metadata sheet
+  dplyr::rename(Timepoint = Date)
+collection.summary$Timepoint <- as.Date(collection.summary$Timepoint, format="%m/%d/%y")
+
+sequencing.id <- read.csv("~/MyProjects/HI_Bleaching_Timeseries/data/16S/metadata//16S_sequencingID.csv", header = TRUE) %>%
+  subset(Project == "ES_BP" & Type == "16S") %>% # selecting for just this 16S project's data and excluding Ariana and Kevin's 
+  dplyr::rename(ColonyID = Coral.ID) %>%
+  select(Sample.ID, ColonyID, Timepoint) %>%
+  mutate(Timepoint = case_when(
+    Timepoint == "2019-07-20" ~ "2019-07-19",
+    Timepoint == "2019-12-04" ~ "2019-12-04"))
+
+sequencing.id$ColonyID <- sub(".","", sequencing.id$ColonyID)
+sequencing.id$ColonyID <- sub(".","", sequencing.id$ColonyID) # do this twice to get rid of both the M and "-" symbol
+  
+collection.summary <- collection.summary %>% unite(Group, ColonyID, Timepoint, sep = " ")
+sequencing.id <- sequencing.id %>% unite(Group, ColonyID, Timepoint, sep = " ")
+
+metadata <- full_join(collection.summary, sequencing.id, by = "Group") %>% na.omit() %>%
+  separate(Group, c("ColonyID", "Year", "Month", "Day")) %>%
+  unite(Timepoint, Year, Month, sep = "-") %>% unite(Timepoint, Timepoint, Day, sep = "-") 
+
+metadata <- metadata %>% rename(`#SampleID` = Sample.ID)
+
+metadata <- metadata[, c(7,1,2,3,4,5,6)] # reordering the columns 
+metadata[1,1] <- "#q2:types"
+metadata[1,2:7] <- "categorical" #adding types of variables for QIIME2 pipeline 
+
+write.table(metadata, "~/MyProjects/HI_Bleaching_Timeseries/data/16S/metadata/metadata.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+
+
+
+  
