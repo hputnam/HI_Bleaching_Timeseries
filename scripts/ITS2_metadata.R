fd4efa02eda2a74fb1605d78e4324e08bfd8d90b
Emma Strand <emma_strand@uri.edu>
2022-01-18 09:58:01 -0500

    metadata ITS2

---
+++ scripts/ITS2_metadata.R
@@ -0,0 +1,3 @@
+## ITS2 Metadata file 
+## notebook post to pipeline: https://github.com/emmastrand/EmmaStrand_Notebook/blob/master/_posts/2022-01-14-KBay-Bleaching-Pairs-ITS2-Analysis-Pipeline.md
+
